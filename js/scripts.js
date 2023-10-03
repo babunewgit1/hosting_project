c2a0b29d6b6a8090bcd480b2efc5d45ce078ac39
babunewgit1 <babunewx11@gmail.com>
2023-10-03 17:17:21 +0600

    finally done

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,18 @@
 (function ($) {
   $(document).ready(function () {
-    // code will write here
+    // Scroll to Top
+    jQuery(".scrolltotop").click(function () {
+      jQuery("html").animate({ scrollTop: "0px" }, 400);
+      return false;
+    });
+
+    jQuery(window).scroll(function () {
+      var upto = jQuery(window).scrollTop();
+      if (upto > 50) {
+        jQuery(".scrolltotop").fadeIn();
+      } else {
+        jQuery(".scrolltotop").fadeOut();
+      }
+    });
   });
 })(jQuery);
